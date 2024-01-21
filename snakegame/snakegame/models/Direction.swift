174c9290fb019e2506c7d315865ef56268e1c15f
Vlad <vlad92z@users.noreply.github.com>
2024-01-21 13:41:20 +0000

    Add a NetworkController and functions to request state and make moves

---
+++ snakegame/snakegame/models/Direction.swift
@@ -0,0 +1,16 @@
+//
+//  Direction.swift
+//  snakegame
+//
+//  Created by Vlad Z on 21/01/2024.
+//
+
+import Foundation
+
+enum Direction: Int {
+    
+    case UP = 0
+    case RIGHT = 1
+    case DOWN = 2
+    case LEFT = 3
+}
---
+++ snakegame/snakegame/models/Point.swift
@@ -0,0 +1,13 @@
+//
+//  Point.swift
+//  snakegame
+//
+//  Created by Vlad Z on 21/01/2024.
+//
+
+import Foundation
+
+struct Point: Codable {
+    var x: Double
+    var y: Double
+}
